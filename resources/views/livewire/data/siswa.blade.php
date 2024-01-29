9733a0f4babd8cd8cce72f62887e55e687a8982c
A Aries Pirnando <ariespirnando@users.noreply.github.com>
2024-01-29 09:53:56 +0700

    Update siswa.blade.php

--- resources/views/livewire/data/siswa.blade.php
+++ resources/views/livewire/data/siswa.blade.php
@@ -70,14 +70,14 @@
                 <div class="text-muted">
                   Show
                   <div class="mx-2 d-inline-block">
-                    <input type="text" class="form-control form-control-sm" value="8" size="3" aria-label="Invoices count">
+                    <input wire:model.live='totalPagging' type="text" class="form-control form-control-sm" value="8" size="3" aria-label="Invoices count">
                   </div>
                   entries
                 </div>
                 <div class="ms-auto text-muted">
                   Search:
                   <div class="ms-2 d-inline-block">
-                    <input type="text" class="form-control form-control-sm" aria-label="Search invoice">
+                    <input wire:model.live='searchData' type="text" class="form-control form-control-sm" aria-label="Search invoice">
                   </div>
                 </div>
               </div>
@@ -92,62 +92,37 @@
                     <th width="10%">NIS</th>
                     <th width="40%">Nama</th>
                     <th width="5%">Jenis Kelamin</th>
-                    <th width="10%">Tempat & Tanggal Lahir</th>
-                    {{-- <th width="20%">Nama Orang Tua</th>
-                    <th width="10%">No Kontak Orang Tua</th>
-                    <th width="40%">Alamat</th> --}}
+                    <th width="10%">Tempat & Tanggal Lahir</th>   
                     <th width="20%">Aksi</th>
                   </tr>
                 </thead>
                 <tbody>
+                  @foreach ($dataSiswa as $key => $values )
                   <tr>
                     <td><span class="text-muted">1</span></td>
                     <td><span class="text-muted">001401</span></td>
                     <td><span class="text-muted">001401</span></td>
-                    <td><span class="text-muted">001401</span></td>
-                    {{-- <td><span class="text-muted">001401</span></td>
-                    <td><span class="text-muted">001401</span></td>
-                    <td><span class="text-muted">001401</span></td> --}}
-                    <td><a href="invoice.html" class="text-reset" tabindex="-1">Design Works</a></td>
+                    <td><span class="text-muted">001401</span></td>  
                     <td>
                         <div class="btn-list flex-nowrap">
-                            <a href="#" class="btn">
+                            <a wire:click='setDetails({{$values->id}})' class="btn" data-bs-toggle="modal" data-bs-target="#modal-report">
                                 Detail
                             </a>
-                            <a href="#" class="btn">
+                            <a wire:click='setEdited({{$values->id}})' class="btn" data-bs-toggle="modal" data-bs-target="#modal-report">
                               Ubah
                             </a>
-                            <a href="#" class="btn" data-bs-toggle="modal" data-bs-target="#modal-simple">
-                                Hapus
+                            <a wire:click='setDeleted({{$values->id}})' class="btn" data-bs-toggle="modal" data-bs-target="#modal-simple">
+                              Hapus
                             </a>
                         </div>
                     </td>
                   </tr>
+                  @endforeach
                 </tbody>
               </table>
             </div>
             <div class="card-footer d-flex align-items-center">
-              <p class="m-0 text-muted">Showing <span>1</span> to <span>8</span> of <span>16</span> entries</p>
-              <ul class="pagination m-0 ms-auto">
-                <li class="page-item disabled">
-                  <a class="page-link" href="#" tabindex="-1" aria-disabled="true">
-                    <!-- Download SVG icon from http://tabler-icons.io/i/chevron-left -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 6l-6 6l6 6" /></svg>
-                    prev
-                  </a>
-                </li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item active"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item"><a class="page-link" href="#">4</a></li>
-                <li class="page-item"><a class="page-link" href="#">5</a></li>
-                <li class="page-item">
-                  <a class="page-link" href="#">
-                    next <!-- Download SVG icon from http://tabler-icons.io/i/chevron-right -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6l6 6l-6 6" /></svg>
-                  </a>
-                </li>
-              </ul>
+              {{ $dataSiswa->links()}}
             </div>
           </div>
         </div>
@@ -157,11 +132,17 @@
 
 
   {{-- MODAL --}}
-  <div class="modal modal-blur fade" id="modal-report" tabindex="-1" role="dialog" aria-hidden="true">
+  <div wire:ignore.self class="modal modal-blur fade" id="modal-report" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
       <div class="modal-content">
-        <div class="modal-header">
+        <div class="modal-header"> 
+          @if ($editMode==true)
+          <h5 class="modal-title">Ubah Data Siswa</h5>
+          @elseif ($detailMode==true)
+          <h5 class="modal-title">Detail Data Siswa</h5>
+          @else
           <h5 class="modal-title">Tambah Siswa</h5>
+          @endif
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
@@ -239,20 +220,28 @@
         </div>
 
         <div class="modal-footer">
-          <a href="#" class="btn btn-link link-secondary" data-bs-dismiss="modal">
+          <a href="#" id="close_form_data" class="btn btn-link link-secondary" data-bs-dismiss="modal">
             Cancel
           </a>
-          <a href="#" class="btn btn-primary ms-auto" data-bs-dismiss="modal">
+          @if ($editMode == true)
+          <a wire:click='update_data()' class="btn btn-primary ms-auto">
+            <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg>
+             Ubah Data Siswa
+          </a>
+          @elseif ($detailMode == false && $editMode== false)
+          <a wire:click='store()' class="btn btn-primary ms-auto">
             <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
             <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
             Simpan Data Siswa
           </a>
+          @endif
         </div>
       </div>
     </div>
   </div>
 
-  <div class="modal modal-blur fade" id="modal-simple" tabindex="-1" role="dialog" aria-hidden="true">
+  <div wire:ignore.self class="modal modal-blur fade" id="modal-simple" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
       <div class="modal-content">
         <div class="modal-header">
@@ -264,12 +253,16 @@
         </div>
         <div class="modal-footer">
           <button type="button" class="btn me-auto" data-bs-dismiss="modal">Tutup</button>
-          <span wire:click='remove()'>
-            <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Hapus</button>
-          </span>
+          <button wire:click="remove_data()" type="button" class="btn btn-primary" data-bs-dismiss="modal">Hapus</button>
         </div>
       </div>
     </div>
   </div>
 
 </div>
+@livewireScripts
+<script>
+Livewire.on('afterProcess', function () {
+    document.getElementById('close_form_data').click();
+});
+</script>
