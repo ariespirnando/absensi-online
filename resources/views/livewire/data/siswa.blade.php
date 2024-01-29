3eaca1c3e8b6de9d2f3e1b9aa171a24589bcc30d
A Aries Pirnando <ariespirnando@users.noreply.github.com>
2024-01-29 09:48:52 +0700

    Update siswa.blade.php

--- resources/views/livewire/data/siswa.blade.php
+++ resources/views/livewire/data/siswa.blade.php
@@ -14,15 +14,40 @@
             Data - Siswa
           </h3>
         </div>
+
+        @if(session('message')) 
+        <div class="alert alert-success alert-dismissible" role="alert">
+        <div class="d-flex">
+            <div>
+            <h4 class="alert-title">Pesan Aksi</h4>
+            <div class="text-secondary">{{ session('message') }}</div>
+            </div>
+        </div>
+        <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+        </div> 
+        @endif
+
+        @if(session('error')) 
+        <div class="alert alert-danger alert-dismissible" role="alert">
+        <div class="d-flex">
+            <div>
+            <h4 class="alert-title">Pesan Aksi</h4>
+            <div class="text-secondary">{{ session('error') }}</div>
+            </div>
+        </div>
+        <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+        </div> 
+        @endif
+          
         <!-- Page title actions -->
         <div class="col-auto ms-auto d-print-none">
           <div class="btn-list">
-            <a href="#" class="btn btn-primary d-none d-sm-inline-block" data-bs-toggle="modal" data-bs-target="#modal-report">
+            <a wire:click='setAdd()' class="btn btn-primary d-none d-sm-inline-block" data-bs-toggle="modal" data-bs-target="#modal-report">
               <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
               <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
               Tambah Data
             </a>
-            <a href="#" class="btn btn-primary d-sm-none btn-icon" data-bs-toggle="modal" data-bs-target="#modal-report" aria-label="Create new report">
+            <a wire:click='setAdd()' class="btn btn-primary d-sm-none btn-icon" data-bs-toggle="modal" data-bs-target="#modal-report" aria-label="Create new report">
               <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
               <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
             </a>
