25c5cf9d5256e819afd0b9fb72d41c03b688332c
A Aries Pirnando <ariespirnando@users.noreply.github.com>
2024-01-29 09:54:27 +0700

    Update pelajaran.blade.php

--- resources/views/livewire/data/pelajaran.blade.php
+++ resources/views/livewire/data/pelajaran.blade.php
@@ -129,7 +129,7 @@
 
 
   {{-- MODAL --}}
-  <div class="modal modal-blur fade" id="modal-report" tabindex="-1" role="dialog" aria-hidden="true">
+  <div wire:ignore.self class="modal modal-blur fade" id="modal-report" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
       <div class="modal-content">
         <div class="modal-header"> 
